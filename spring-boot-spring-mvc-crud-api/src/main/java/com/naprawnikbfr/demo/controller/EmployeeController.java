a5d469fcff73f3430e92b30a1c074636372aea49
BLAZEJ FRACZEK <fraczek.blazej@gmail.com>
2023-05-26 16:47:08 +0200

    Commit after adding 'update' feature.

--- spring-boot-spring-mvc-crud-api/src/main/java/com/naprawnikbfr/demo/controller/EmployeeController.java
+++ spring-boot-spring-mvc-crud-api/src/main/java/com/naprawnikbfr/demo/controller/EmployeeController.java
@@ -5,10 +5,7 @@ import com.naprawnikbfr.demo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/employees")
@@ -50,5 +47,18 @@ public class EmployeeController {
 
         //use a redirect to prevent multiple submissions;
         return "redirect:/employees/list";
+    }
+
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("employeeId") int theId, Model theModel){
+
+        //get employee from service
+        Employee dbEmployee = employeeService.findById(theId);
+
+        //set dbEmployee in the model to prepopulate the form
+        theModel.addAttribute("employee", dbEmployee);
+
+        //send over to our form
+        return "employees/employee-form";
     }
 }
